b231780b3831582bcc9be80ab45d843273430edc
Oliver Cawson <cawsonoliver33@gmail.com>
2023-08-18 00:12:00 +0800

    feat/add search bar

--- apps/web/components/timetokens-wallet/CustomExpertTable.tsx
+++ apps/web/components/timetokens-wallet/CustomExpertTable.tsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { trpc } from "@calcom/trpc/react";
 import {
@@ -11,117 +13,135 @@ import {
   DropdownMenuTrigger,
   Tooltip,
   showToast,
+  Input,
+  EmptyScreen,
 } from "@calcom/ui";
-import { MoreHorizontal, Trash2, ShoppingCart } from "@calcom/ui/components/icon";
+import { MoreHorizontal, Trash2, ShoppingCart, Newspaper } from "@calcom/ui/components/icon";
 
-function CustomExpertTable() {
+export interface ExpertDataType {
+  email: string;
+  fullname: string;
+  expert_token_amount: number;
+  token_amount: number;
+  token_price: number;
+  added: boolean;
+}
+
+interface CustomExpertTableProps {
+  expertsData: ExpertData[];
+  columns: string[];
+  handleBuyEvent: (email: string, tokens: number) => void;
+}
+
+function CustomExpertTable(props: CustomExpertTableProps) {
   const { t } = useLocale();
   const [user] = trpc.viewer.me.useSuspenseQuery();
 
-  const mockupData = [
-    {
-      fullname: "Expert 1",
-      token_amount: 200,
-      token_price: 5,
-    },
-    {
-      fullname: "Expert 2",
-      token_amount: 200,
-      token_price: 5,
-    },
-    {
-      fullname: "Expert 3",
-      token_amount: 0,
-      token_price: 5,
-    },
-    {
-      fullname: "Expert 4",
-      token_amount: 0,
-      token_price: 5,
-    },
-    {
-      fullname: "Expert 5",
-      token_amount: 200,
-      token_price: 5,
-    },
-    {
-      fullname: "Expert 6",
-      token_amount: 200,
-      token_price: 5,
-    },
-  ];
+  const [tokensAmount, setTokensAmount] = useState<number[]>([]);
 
-  const columns = ["Expert", "Tokens amount", "Token price", ""];
+  useEffect(() => {
+    const data: number[] = [];
+    data.fill(10, 0, expertsData.length);
+    setTokensAmount(data);
+  }, []);
+
+  const { expertsData, columns, handleBuyEvent } = props;
 
   return (
-    <table className="w-2/3 border-collapse">
-      <thead>
-        <tr>
-          {columns.map((column) => (
-            <th className="px-4 py-2 text-left" key={column}>
-              {column}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {mockupData.map((data) => (
-          <tr key={data.fullname}>
-            <td className="px-4 py-2">
-              <div className="flex items-center">
-                <Avatar className="mr-2" alt="Nameless" size="sm" imageSrc="" />
-                {data.fullname}
-              </div>
-            </td>
-            <td className="px-4 py-2">{data.token_amount}</td>
-            <td className="px-4 py-2">{data.token_price}</td>
-            <td className="px-4 py-2">
-              <ButtonGroup combined>
-                {true && (
-                  <>
-                    <Tooltip content={t("buy_expert_tokens")}>
-                      <Button
-                        data-testid="preview-link-button"
-                        color="secondary"
-                        target="_blank"
-                        variant="icon"
-                        href=""
-                        StartIcon={ShoppingCart}
-                      />
-                    </Tooltip>
-                  </>
-                )}
-                <Dropdown modal={false}>
-                  <DropdownMenuTrigger asChild data-testid="event-type-options-">
-                    <Button
-                      type="button"
-                      variant="icon"
-                      color="secondary"
-                      StartIcon={MoreHorizontal}
-                      className="ltr:radix-state-open:rounded-r-md rtl:radix-state-open:rounded-l-md"
+    <div>
+      {expertsData.length === 0 && (
+        <div className="w-full px-4 lg:w-2/3">
+          <EmptyScreen
+            Icon={Newspaper}
+            headline={t("no_expert_data")}
+            description={t("no_expert_data_description")}
+          />
+        </div>
+      )}
+      {expertsData.length > 0 && (
+        <table className="w-full border-collapse lg:w-2/3">
+          <thead>
+            <tr>
+              {columns.map((column) => (
+                <th className="px-4 py-2 text-left" key={column}>
+                  {column}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {expertsData.map((data, index) => (
+              <tr key={data.fullname} className="">
+                <td className="px-4 py-2">
+                  <div className="flex items-center">
+                    <Avatar className="mr-2" alt="Nameless" size="sm" imageSrc="" />
+                    {data.fullname}
+                  </div>
+                </td>
+                <td className="px-4 py-2">{data.token_amount}</td>
+                <td className="px-4 py-2">{data.token_price}</td>
+                <td className="flex justify-end px-4 py-2">
+                  <ButtonGroup combined>
+                    <Input
+                      type="number"
+                      min={1}
+                      disabled={false}
+                      onChange={(e) => {
+                        const val = Number(e.target?.value);
+                        const data = tokensAmount;
+                        data[index] = val;
+                        setTokensAmount(data);
+                      }}
+                      className="max-w-24 border-default text-sm [appearance:textfield]"
+                      defaultValue={10}
                     />
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent>
                     {true && (
-                      <DropdownMenuItem>
-                        <DropdownItem
-                          type="button"
-                          data-testid="event-type-edit-"
-                          disabled={data.token_amount === 0}
-                          StartIcon={Trash2}
-                          onClick={() => showToast(t("remove"), "error")}>
-                          {t("remove")}
-                        </DropdownItem>
-                      </DropdownMenuItem>
+                      <>
+                        <Tooltip content={t("buy_expert_tokens")}>
+                          <Button
+                            data-testid="preview-link-button"
+                            color="secondary"
+                            target="_blank"
+                            variant="icon"
+                            onClick={() => handleBuyEvent(data.email, tokensAmount[index])}
+                            StartIcon={ShoppingCart}
+                          />
+                        </Tooltip>
+                      </>
                     )}
-                  </DropdownMenuContent>
-                </Dropdown>
-              </ButtonGroup>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+                    <Dropdown modal={false}>
+                      <DropdownMenuTrigger asChild data-testid="event-type-options-">
+                        <Button
+                          type="button"
+                          variant="icon"
+                          color="secondary"
+                          StartIcon={MoreHorizontal}
+                          className="ltr:radix-state-open:rounded-r-md rtl:radix-state-open:rounded-l-md"
+                        />
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                        {true && (
+                          <DropdownMenuItem>
+                            <DropdownItem
+                              type="button"
+                              data-testid="event-type-edit-"
+                              disabled={data.token_amount === 0}
+                              StartIcon={Trash2}
+                              onClick={() => showToast(t("remove"), "error")}>
+                              {t("remove")}
+                            </DropdownItem>
+                          </DropdownMenuItem>
+                        )}
+                      </DropdownMenuContent>
+                    </Dropdown>
+                  </ButtonGroup>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
   );
 }
 
--- apps/web/pages/timetokens-wallet/index.tsx
+++ apps/web/pages/timetokens-wallet/index.tsx
@@ -1,42 +1,179 @@
 import type { GetServerSidePropsContext } from "next";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { components } from "react-select";
 
 import { getLayout } from "@calcom/features/MainLayout";
 import { ShellMain } from "@calcom/features/shell/Shell";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { trpc } from "@calcom/trpc/react";
+import { Select, Button, Avatar, Badge, ConfirmationDialogContent, Dialog } from "@calcom/ui";
+import { Plus } from "@calcom/ui/components/icon";
 
 import PageWrapper from "@components/PageWrapper";
+import CustomExpertTable from "@components/timetokens-wallet/CustomExpertTable";
 
 import { ssrInit } from "@server/lib/ssr";
-
-import CustomExpertTable from "../../components/timetokens-wallet/CustomExpertTable";
 
 function TimeTokens() {
   const { t } = useLocale();
   const [user] = trpc.viewer.me.useSuspenseQuery();
 
+  const [addedExpertsData, setAddedExpertsData] = useState([]);
+  const [buyConfirmOpen, setBuyConfirmOpen] = useState(false);
+  const [buyExpertEmail, setBuyExpertEmail] = useState("");
+  const [buyTokensAmount, setBuyTokensAmount] = useState(0);
+  const [expertSearchResult, setExpertSearchResult] = useState([]);
+  const [expertOptions, setExpertOptions] = useState([]);
+  const [addExpertEmail, setAddExpertEmail] = useState<string>("");
+
   const mockupData = [
     {
+      email: "cawsonoliver33@gmail.com",
       fullname: "Expert 1",
+      expert_token_amount: 1000,
       token_amount: 200,
       token_price: 5,
+      added: false,
     },
     {
+      email: "cawsonoliver44@gmail.com",
       fullname: "Expert 2",
+      expert_token_amount: 1000,
       token_amount: 200,
       token_price: 5,
+      added: false,
     },
     {
+      email: "cawsonoliver55@gmail.com",
       fullname: "Expert 3",
+      expert_token_amount: 1000,
+      token_amount: 0,
+      token_price: 5,
+      added: true,
+    },
+    {
+      email: "cawsonoliver66@gmail.com",
+      fullname: "Expert 4",
+      expert_token_amount: 1000,
+      token_amount: 0,
+      token_price: 5,
+      added: true,
+    },
+    {
+      email: "cawsonoliver77@gmail.com",
+      fullname: "Expert 5",
+      expert_token_amount: 1000,
       token_amount: 200,
       token_price: 5,
+      added: false,
+    },
+    {
+      email: "cawsonoliver88@gmail.com",
+      fullname: "Expert 6",
+      token_amount: 200,
+      token_price: 5,
+      added: false,
     },
   ];
 
+  const columns = ["Expert", "Tokens amount", "Token price", ""];
+
+  useEffect(() => {
+    if (expertSearchResult.length === 0) {
+      setAddedExpertsData([]);
+      setExpertSearchResult(mockupData);
+      changeExpertOptions(mockupData);
+    }
+
+    console.log(addedExpertsData);
+  }, []);
+
+  const handleBuyEvent = (email: string, tokens: number) => {
+    setBuyConfirmOpen(true);
+    setBuyExpertEmail(email);
+    setBuyTokensAmount(tokens);
+  };
+
+  const changeExpertOptions = (searchResult) => {
+    const data = [];
+    for (const expert of searchResult) {
+      data.push({
+        label: expert.fullname,
+        value: expert.email,
+        added: expert.added,
+      });
+    }
+
+    setExpertOptions(data);
+    console.log(data);
+  };
+
+  const CustomOption = ({ icon, label, added }) => {
+    return (
+      <div className="flex items-center">
+        <Avatar className="mr-2" alt="Nameless" size="sm" imageSrc={icon} />
+        <span>{label}</span>
+        {added && (
+          <Badge className="ml-auto" size="md" variant="gray">
+            {t("added")}
+          </Badge>
+        )}
+      </div>
+    );
+  };
+
+  const addExpert = () => {
+    const data = addedExpertsData;
+
+    for (const expert of expertSearchResult) {
+      if (expert?.email === addExpertEmail) {
+        data.push(expert);
+        break;
+      }
+    }
+
+    setAddedExpertsData(data);
+  };
+
   return (
     <ShellMain heading={t("timetokens_wallet")} hideHeadingOnMobile subtitle={t("buy_sell_timetokens")}>
-      <CustomExpertTable />
+      <div className="mb-4 flex w-full items-center justify-center gap-4 px-4 lg:w-2/3">
+        <Select
+          options={expertOptions}
+          components={{
+            Option: (props) => {
+              return (
+                <components.Option {...props}>
+                  <CustomOption icon={null} label={props.data.label} added={props.data.added} />
+                </components.Option>
+              );
+            },
+          }}
+          isSearchable={true}
+          className="w-full rounded-md text-sm"
+          onChange={(event) => {
+            setAddExpertEmail(event?.value);
+          }}
+        />
+        <Button disabled={addExpertEmail === ""} onClick={addExpert} data-testid="" StartIcon={Plus}>
+          {t("add")}
+        </Button>
+      </div>
+      <CustomExpertTable columns={columns} expertsData={addedExpertsData} handleBuyEvent={handleBuyEvent} />
+      <Dialog open={buyConfirmOpen} onOpenChange={setBuyConfirmOpen}>
+        <ConfirmationDialogContent
+          variety="danger"
+          title="Confirmation"
+          confirmBtnText={t(`confirm_buy_event`)}
+          loadingText={t(`confirm_buy_event`)}
+          onConfirm={(e) => {
+            e.preventDefault();
+            console.log(buyExpertEmail, buyTokensAmount);
+            setBuyConfirmOpen(false);
+          }}>
+          <p className="mt-5">Do you want to really buy tokens?</p>
+        </ConfirmationDialogContent>
+      </Dialog>
     </ShellMain>
   );
 }
