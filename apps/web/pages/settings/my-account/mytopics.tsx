c0e16b9316f41b3dffaf58b909bed4ed09c9b2e8
SnowFlake <snowflake305188@gmail.com>
2023-10-04 14:18:32 +0000

    feat: my topics ui

--- apps/web/pages/settings/my-account/mytopics.tsx
+++ apps/web/pages/settings/my-account/mytopics.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import { getLayout } from "@calcom/features/settings/layouts/SettingsLayout";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
-import { Button, Meta } from "@calcom/ui";
+import { Button, Dialog, DialogContent, TextArea, Meta } from "@calcom/ui";
 import { Plus, Pencil, Trash } from "@calcom/ui/components/icon";
 
 import PageWrapper from "@components/PageWrapper";
@@ -13,16 +13,18 @@ interface ITopic {
 }
 const TopicCard = ({ topicName, topicDesc }: ITopic) => {
   const { t } = useLocale();
+
   return (
-    <div className="flex justify-between justify-items-center p-[20px]">
+    <div className="flex justify-between justify-items-center rounded-none border border-solid border-[#E5E7EB] bg-[#6D278E0D] p-[20px]">
       <div className="flex justify-items-start text-sm font-bold leading-4 text-[#101010]">
         <p>{topicName}</p>
       </div>
       <div className="w-[300px]">
         <p className="text-sm leading-5 text-[#6B7280]">{topicDesc}</p>
       </div>
-      <div className="flex justify-between px-[20px] py-[10px] gap-[13px]">
+      <div className="flex justify-between gap-[13px] px-[20px] py-[10px]">
         <Button
+          className="flex gap-[11px] p-[10px] text-sm leading-4 shadow-md"
           color="secondary"
           onClick={() => {
             console.log("edit");
@@ -31,6 +33,7 @@ const TopicCard = ({ topicName, topicDesc }: ITopic) => {
           {t("edit")}
         </Button>
         <Button
+          className="flex gap-[11px] p-[10px] text-sm leading-4 shadow-md"
           color="secondary"
           onClick={() => {
             console.log("remove");
@@ -45,6 +48,7 @@ const TopicCard = ({ topicName, topicDesc }: ITopic) => {
 
 const TopicsView = () => {
   const { t } = useLocale();
+  const [isOpen, setIsOpen] = useState(false);
   const [topics, setTopics] = useState<ITopic[]>([
     {
       topicName: "Topic1",
@@ -61,29 +65,60 @@ const TopicsView = () => {
   ]);
 
   return (
-    <div className="flex flex-col">
-      <Meta
-        title={t("microcard_description")}
-        description=" "
-        CTA={
+    <>
+      <div className="flex flex-col gap-[30px]">
+        <Meta
+          title={t("microcard_description")}
+          description=" "
+          CTA={
+            <Button
+              color="primary"
+              StartIcon={Plus}
+              className="h-[40px] w-[40px] !rounded-full"
+              variant="icon"
+              onClick={() => {
+                setIsOpen(true);
+              }}
+            />
+          }
+        />
+        <div className="flex flex-col gap-[10px]">
+          {topics.map((topic, key) => (
+            <TopicCard key={key} {...topic} />
+          ))}
+        </div>
+        <div>
           <Button
             color="primary"
-            StartIcon={Plus}
-            className="h-[40px] w-[40px] !rounded-full"
-            variant="icon"
-            onClick={() => {
-              console.log("click plus");
-            }}
-          />
-        }
-      />
-      <div className="flex flex-col gap-[10px]">
-        {topics.map((topic, key) => (
-          <TopicCard key={key} {...topic} />
-        ))}
+            className="flex h-[36px] w-[80px] justify-center p-[6.166px] text-[12.332px] leading-[17.264px]">
+            {t("save")}
+          </Button>
+        </div>
       </div>
-      <Button color="primary">{t("save")}</Button>
-    </div>
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogContent title={t("add_topic")} type="creation">
+          <div className="flex flex-col gap-[29.6px] rounded-[6.166px] border-[0.617px] border-solid border-[#E5E7EB] p-[30.83px]">
+            <div className="flex flex-col gap-[18.5px]">
+              <div className="text-[14.798px] leading-[17.264px]">{t("title")}</div>
+              <TextArea
+                id="topic_title"
+                placeholder={t("form_description_placeholder")}
+                rows={5}
+                className="rounded-[3.083px] border-[0.617px] border-[#C4C4C4]"
+              />
+            </div>
+            <div className="flex justify-end">
+              <Button
+                color="primary"
+                onClick={() => console.log("click")}
+                className="flex h-[36px] w-[80px] justify-center p-[6.166px]">
+                {t("save")}
+              </Button>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
