e988a756ae8220d71a8f88835ee747a1e5d31b4d
SnowFlake <snowflake305188@gmail.com>
2023-10-05 13:36:58 +0000

    fix: QDRANT_URL from .env

--- apps/web/pages/settings/my-account/mytopics.tsx
+++ apps/web/pages/settings/my-account/mytopics.tsx
@@ -19,8 +19,8 @@ import { Plus, Pencil, Trash2, Newspaper } from "@calcom/ui/components/icon";
 
 import PageWrapper from "@components/PageWrapper";
 
-const QDRANT_URL = "http://159.89.5.59:6333";
-const COLLECTION_NAME = "topics";
+const QDRANT_URL = process.env.NEXT_PUBLIC_QDRANT_URL;
+const COLLECTION_NAME = process.env.NEXT_PUBLIC_COLLECTION_NAME || "topics";
 
 interface ITopic {
   id: string;
