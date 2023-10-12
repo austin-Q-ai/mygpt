773d9a29440d76979f000f3d7344e2473a32a356
enthusiastica <williamcambell682@gmail.com>
2023-10-12 15:21:12 +0000

    feat: ui update for bots

---
+++ apps/web/pages/settings/bots/expert.tsx
@@ -0,0 +1,12 @@
+import { getLayout } from "@calcom/features/settings/layouts/SettingsLayout";
+
+import PageWrapper from "@components/PageWrapper";
+
+const ExpertView = () => {
+  return <></>;
+};
+
+ExpertView.getLayout = getLayout;
+ExpertView.PageWrapper = PageWrapper;
+
+export default ExpertView;
---
+++ apps/web/pages/settings/bots/telegram-bot.tsx
@@ -0,0 +1,12 @@
+import { getLayout } from "@calcom/features/settings/layouts/SettingsLayout";
+
+import PageWrapper from "@components/PageWrapper";
+
+const TelegramBotView = () => {
+  return <></>;
+};
+
+TelegramBotView.getLayout = getLayout;
+TelegramBotView.PageWrapper = PageWrapper;
+
+export default TelegramBotView;
