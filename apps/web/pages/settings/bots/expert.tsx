2ac59aff14d899565ecfead1397c42d8616d0e6f
enthusiastica <williamcambell682@gmail.com>
2023-10-13 01:17:49 +0000

    feat: add apiKey, expertId

--- apps/web/pages/settings/bots/expert.tsx
+++ apps/web/pages/settings/bots/expert.tsx
@@ -1,8 +1,27 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 import { getLayout } from "@calcom/features/settings/layouts/SettingsLayout";
+import { trpc } from "@calcom/trpc/react";
 
 import PageWrapper from "@components/PageWrapper";
 
 const ExpertView = () => {
+  const { data: user } = trpc.viewer.me.useQuery();
+  const mutation = trpc.viewer.updateProfile.useMutation();
+  useEffect(() => {
+    axios
+      .get(`http://104.248.16.57:5050/brains/default/`, {
+        headers: {
+          Authorization: `Bearer ${user?.apiKey}`,
+        },
+      })
+      .then((data) => {
+        mutation.mutate({
+          expertId: data.data.id,
+        });
+      });
+  }, []);
   return <></>;
 };
 
