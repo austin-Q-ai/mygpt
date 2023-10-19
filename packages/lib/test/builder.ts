08d243e96352c10d984e376b5e1b7ddea1e7804c
Oliver Cawson <cawsonoliver33@gmail.com>
2023-10-19 00:59:05 +0000

    fix: type errors in builder.ts

--- packages/lib/test/builder.ts
+++ packages/lib/test/builder.ts
@@ -197,6 +197,7 @@ export const buildUser = <T extends Partial<UserPayload>>(user?: T): UserPayload
     avatar: "",
     away: false,
     bio: null,
+    videoCloneToken: null,
     brandColor: "#292929",
     bufferTime: 0,
     completedOnboarding: false,
