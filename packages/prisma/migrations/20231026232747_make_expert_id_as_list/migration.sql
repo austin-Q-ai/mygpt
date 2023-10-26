814e7b05f29a1de1339f8133637e3847203cc83e
enthusiastica <williamcambell682@gmail.com>
2023-10-26 23:28:43 +0000

    fix: db-migrate for make expertId as list

---
+++ packages/prisma/migrations/20231026232747_make_expert_id_as_list/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - The `expertId` column on the `users` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "expertId",
+ADD COLUMN     "expertId" TEXT[];
